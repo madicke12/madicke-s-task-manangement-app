e38b7e98f922115e541806f17a8fd65c23e112e7
madicke12 <cissemadicke8@gmail.com>
2023-12-08 00:07:41 +0000

    implemented dynamic inputs creation

--- task_management_by_madicke/src/app/board/dynamic-input.jsx
+++ task_management_by_madicke/src/app/board/dynamic-input.jsx
@@ -1,35 +1,29 @@
-'use client'
+"use client";
 
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, TrashIcon } from "@heroicons/react/24/solid";
 
-import { useState } from 'react';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import {PlusIcon ,TrashIcon} from '@heroicons/react/24/solid'
-const DynamicInput = ({type}) => {
-  const [inputValue , setInputValue] = useState()
-  const [inputCount, setInputCount] = useState([{id:Date.now()*2 ,value:null}]);
+const DynamicInput = ({ type }) => {
+  const [inputCount, setInputCount] = useState([
+    { id: Date.now() * 2, value: "" },
+  ]);
 
-  const handleChange =(id,e)=>{
-    
-    console.log(id)
-    setInputValue(e.target.value)
-    inputCount.forEach((item)=>{
-      if(item.id === id ){
-        setInputCount((prev)=>{
-          prev.forEach()
-        })
+  const handleChange = (id, e) => {
+    const updatedInputs = inputCount.map((item) => {
+      if (item.id === id) {
+        return { ...item, value: e.target.value };
       }
-    })
-    setInputCount((prev )=>{
-      prev.forEach((item)=>{
-       { item.value= inputValue }
-      })
-    })
-    console.log(inputValue)
-    console.log(inputCount)
-  }
+      return item;
+    });
+    console.log(updatedInputs);
+
+    setInputCount(updatedInputs);
+  };
+
   const handleAddInput = () => {
-    setInputCount((prev) => [...prev, { id: Date.now() }]);
+    setInputCount((prev) => [...prev, { id: Date.now(), value: "" }]);
   };
 
   const handleDeleteInput = (id) => {
@@ -37,28 +31,29 @@ const DynamicInput = ({type}) => {
   };
 
   return (
-    <div >
-
+    <div>
       {inputCount.map((input) => (
-        
-       <div className="flex" key={input.id}>
-        {console.log(input.id)}
-       <Input className="mb-2" id={input.id} onChange={(e)=>handleChange(input.id,e)} />{" "}
-       <Button type="button" onClick={()=>handleDeleteInput(input.id)} >
-         {" "}
-         <TrashIcon height={20} />
-       </Button>{" "}
-     </div>
+        <div className="flex" key={input.id}>
+          <Input
+            className="mb-2"
+            id={input.id}
+            value={input.value}
+            onChange={(e) => handleChange(input.id, e)}
+          />
+          <Button type="button" onClick={() => handleDeleteInput(input.id)}>
+            <TrashIcon height={20} />
+          </Button>
+        </div>
       ))}
-      
+
       <Button
-              type="button"
-              onClick={handleAddInput}
-              className="hover:bg-white w-full hover:text-black"
-            >
-              <PlusIcon height={20} />
-              {type === 'columns'? 'Add column' : 'Add Subtask'}
-            </Button>
+        type="button"
+        onClick={handleAddInput}
+        className="hover:bg-white w-full hover:text-black"
+      >
+        <PlusIcon height={20} />
+        {type === "columns" ? "Add column" : "Add Subtask"}
+      </Button>
     </div>
   );
 };
