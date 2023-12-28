5779f185ac41d29b6f16369b737939679556491e
madicke12 <cissemadicke8@gmail.com>
2023-12-28 22:55:18 +0000

    added the form and submit button

--- task_management_by_madicke/src/app/board/taskStatusSelect.jsx
+++ task_management_by_madicke/src/app/board/taskStatusSelect.jsx
@@ -1,27 +1,30 @@
-
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { changeTaskStatus } from "../actions/submit";
+import { Button } from "@/components/ui/button";
 
-const value = ["To Do", "Doing", "Done"];  
-export const TaskStatusSelect = () => {
-    const SelectElements = value.map((item) => (
-        <SelectItem key={item} value={item}>
-          {item}
-        </SelectItem>
-      ));
+const value = ["To Do", "Doing", "Done"];
+export const TaskStatusSelect = ({cisse}) => {
+  const SelectElements = value.map((item) => (
+    <SelectItem key={item} value={item}>
+      {item}
+    </SelectItem>
+  ));
   return (
-   <Select name="status">
-    <SelectTrigger className="w-full">
-      <SelectValue placeholder="chose the status" />
-    </SelectTrigger>
-    <SelectContent>{SelectElements}</SelectContent>
-  </Select>
-  )
-}
-
-  
+    <form action={changeTaskStatus}>
+      <Select name="status">
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="chose the status" />
+        </SelectTrigger>
+        <SelectContent>{SelectElements}</SelectContent>
+      </Select>
+      <input type="text" value={cisse.id} name="taskId" className="hidden"/>
+      <Button type='submit'>save</Button>
+    </form>
+  );
+};
